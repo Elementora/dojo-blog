2eb52ef65ea4e8fad5d01cc8fd200a7df46dff2b
Ersan <ersanaksahin@gmail.com>
2023-03-07 07:00:45 +0300

    first commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+const Home = () => {
+
+    const handleClick = (e) =>{
+
+        console.log(`hello,elementler`,e);
+    }
+
+
+    const handleClickAgain = (name,e) =>{
+
+        console.log(`hello,` + name,e.target);
+    }
+ return ( 
+        <div className="home">
+            <h2>Homepage</h2> 
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) =>handleClickAgain(`mario`,e)}>Click Me Again</button>
+        </div>
+
+ );
+}
+ 
+export default Home;
